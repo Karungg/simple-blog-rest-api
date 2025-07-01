dea8e71edd1994e3e85534a29722629541d7ffb9
Karungg <miftahfadilah71@gmail.com>
2025-07-01 09:17:49 +0700

    Initial commit

---
+++ src/test/java/simpleblogrestapi/simple_blog_rest_api/SimpleBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package simpleblogrestapi.simple_blog_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
