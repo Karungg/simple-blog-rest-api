caf1797e5abb77b1886636f3984e044421a8abee
Karungg <miftahfadilah71@gmail.com>
2025-07-03 08:24:53 +0700

    Add controller advice

--- src/main/java/simpleblogrestapi/simple_blog_rest_api/controller/ErrorController.java
+++ src/main/java/simpleblogrestapi/simple_blog_rest_api/controller/ErrorController.java
@@ -1,8 +1,16 @@
 package simpleblogrestapi.simple_blog_rest_api.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @ControllerAdvice
 public class ErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body("Error " + exception.getMessage());
+    }
 
 }
